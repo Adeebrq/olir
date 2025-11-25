d5f7ad85747397764b36b5063dd62285508d39f0
Adeeb Quraishi <adeebrq@gmail.com>
2025-11-25 23:58:59 +0530

    feat(landing page): add landing page sections

--- Excellent-React/src/Pages/LandingPage/LandingPage.jsx
+++ Excellent-React/src/Pages/LandingPage/LandingPage.jsx
@@ -1,10 +1,18 @@
 import "./LandingPage.css";
-import SEO from "../utils/SEO";
+import HospitalHeroSection from "../../Components/heroSection";
+import WhyChooseUs from "../../Components/chooseUs";
+import PatientTools from "../../Components/patientTools";
+import Testimonials from "../../Components/testimonials";
+
 
 const LandingPage = () => {
   return (<>
     {/* <SEO description={LANDINGPAGE_DESC} title={LANDINGPAGE_TITLE} keywords={LANDINGPAGE_KEYWORDS}/> */}
     <div className="universal-component-wrapper">
+      <HospitalHeroSection />
+      <       WhyChooseUs />
+      <PatientTools/>
+      <Testimonials/>
 
     </div>
   </>
